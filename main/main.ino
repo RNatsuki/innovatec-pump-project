9683ddb5001a835ed741354b151bdd820b0a0034
Ibarra <98603993+RNatsuki@users.noreply.github.com>
2024-04-17 16:12:31 -0600

    Fix formatting and add test code in main.ino

--- main/main.ino
+++ main/main.ino
@@ -7,13 +7,13 @@
  */
 const int PUMP_PIN = 12;                       // Pin to which the pump is connected
 const int PUMP_LED_PIN = 13;                   // Pin to which the pump LED is connected
-const unsigned long PUMP_ON_DURATION = 5000;  // Duration for which the pump should be on
+const unsigned long PUMP_ON_DURATION = 5000;   // Duration for which the pump should be on
 const unsigned long PUMP_OFF_INTERVAL = 10000; // Interval between pump on/off cycles
 
 /*
  * Global objects
  */
-WaterPump waterPump(PUMP_PIN);                                    // Create a WaterPump object
+WaterPump waterPump(PUMP_PIN);                                       // Create a WaterPump object
 Scheduler scheduler(waterPump, PUMP_ON_DURATION, PUMP_OFF_INTERVAL); // Create a Scheduler object
 
 /*
@@ -25,16 +25,12 @@ void setup()
   // Initialize the pump and pump LED pins
   pinMode(PUMP_LED_PIN, OUTPUT);
 
-  /*
-   * Blink the pump LED 5 times to indicate that the system is ready
-   */
-  for (int i = 0; i < 5; i++)
-  {
-    digitalWrite(PUMP_LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(PUMP_LED_PIN, LOW);
-    delay(500);
-  }
+  // Turn on pump for 10 seconds for testing
+  waterPump.turnOn();
+  digitalWrite(PUMP_LED_PIN, HIGH);
+  delay(10000);
+  waterPump.turnOff();
+  digitalWrite(PUMP_LED_PIN, LOW);
 }
 
 /*
